375442b92a0dc97a11b144d320436b3420559480
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 13:00:39 -0500

    commented out the @decorator.oauth_required

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class Invite(ndb.Model):
 
 
 class MainPage(webapp2.RequestHandler):
-    @decorator.oauth_required
+    # @decorator.oauth_required
     def get(self):
         user = users.get_current_user()
         template = JINJA_ENVIRONMENT.get_template('templates/index.html')
@@ -76,13 +76,13 @@ class MainPage(webapp2.RequestHandler):
         }
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render(data))
-        try:
-            http = decorator.http()
-            calendarList = service.calendarList().list(pageToken=None).execute(http=http)
-            for calendar_list_entry in calendarList['items']:
-                print(calendar_list_entry['summary'])
-        except client.AccessTokenRefreshError:
-            self.redirect(decorator.authorize_url())
+        # try:
+        #     http = decorator.http()
+        #     calendarList = service.calendarList().list(pageToken=None).execute(http=http)
+        #     for calendar_list_entry in calendarList['items']:
+        #         print(calendar_list_entry['summary'])
+        # except client.AccessTokenRefreshError:
+        #     self.redirect(decorator.authorize_url())
 
 class InvitePage(webapp2.RequestHandler):
     def get(self):
