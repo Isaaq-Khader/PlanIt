356dc1075d033594a87821dec53cb1db8946d4f2
Isaaq <isaaqkhader@uchicago.edu>
2019-07-22 20:18:48 -0500

    Added in the website url for PlanIt!

--- main.py
+++ main.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# WEBSITE: https://planit-247516.appspot.com
+
 from google.appengine.ext import vendor
 
 # Add any libraries install in the "lib" folder.
